ea031b4e1b8c2aeb43a58e2da8a16bceea9563fb
Yağız BULDUK <146875739+Ygz312629@users.noreply.github.com>
2026-04-14 20:45:55 +0300

    Create logic.py

---
+++ logic.py
@@ -0,0 +1,28 @@
+import sqlite3
+from config import DATABASE
+
+class DB_Manager:
+    def __init__(self, database):
+        self.database = database
+        
+    def create_tables(self):
+        conn = sqlite3.connect(self.database)
+        with conn:
+            conn.execute('''CREATE TABLE IF NOT EXISTS tasks(id, task_name, description, task_time)''') 
+            conn.commit()
+
+    def __executemany(self, sql, data):
+        conn = sqlite3.connect(self.database)
+        with conn:
+            conn.executemany(sql, data)
+            conn.commit()
+    
+    def __select_data(self, sql, data = tuple()):
+        conn = sqlite3.connect(self.database)
+        with conn:
+            cur = conn.cursor()
+            cur.execute(sql, data)
+            return cur.fetchall()
+            
+if __name__ == '__main__':
+    manager = DB_Manager(DATABASE)
